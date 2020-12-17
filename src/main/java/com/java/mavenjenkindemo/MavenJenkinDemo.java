3183b836dd5b0273c1b2f74bb579fb0cc3e5b06e
sumi2222 <sumiboby@yahoo.ca>
2020-12-16 22:35:02 -0500

    first commit

---
+++ src/main/java/com/java/mavenjenkindemo/MavenJenkinDemo.java
@@ -0,0 +1,7 @@
+package com.java.mavenjenkindemo;
+
+public class MavenJenkinDemo {
+	public static void main(String[] args) {
+		System.out.println( " **********   This is my first Jenkin Maven Integration!  Hello World!   *********** " );
+	}
+}
