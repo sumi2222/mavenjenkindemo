3f80e3aa8f09db3df035eb1c12f96db03dddc56d
sumi2222 <sumiboby@yahoo.ca>
2020-12-20 11:41:59 -0500

    Update MavenJenkinDemo.java

--- src/main/java/com/java/mavenjenkindemo/MavenJenkinDemo.java
+++ src/main/java/com/java/mavenjenkindemo/MavenJenkinDemo.java
@@ -2,6 +2,6 @@ package com.java.mavenjenkindemo;
 
 public class MavenJenkinDemo {
 	public static void main(String[] args) {
-		System.out.println( " **********   This is my first github Jenkin Maven pipeline integration!  Hello World!   *********** " );
+		System.out.println( " **********   This is my first github Jenkin Maven pipeline integration is successful!  Hello World!   *********** " );
 	}
 }
